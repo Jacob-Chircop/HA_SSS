ddda7d3c66c0458e6afc499e7d3e98e68949fdae
JacobChircop <jacob.chircop.g53945@mcast.edu.mt>
2025-02-28 16:08:12 +0000

    Added basic routes

--- sssHomeAApp/routes/web.php
+++ sssHomeAApp/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CollegeController;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/colleges', [CollegeController::class, 'index'])->name('colleges.index');
+
+Route::get('/colleges/create', [CollegeController::class, 'create'])->name('colleges.create');
+
+Route::get('/colleges/{id}/edit', [CollegeController::class, 'edit'])->name('colleges.edit');
+
+Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+
+Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+
+Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+
+Route::get('students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
